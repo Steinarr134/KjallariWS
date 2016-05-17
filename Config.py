b2dc819a6e59b43c996fad1ee523fdc2869b1e0b
Steinarr134 <steinarrrrrrrrrr@gmail.com>
2016-05-17 22:43:20 +0000

    Byrjun á páfanum

---
+++ Config.py
@@ -0,0 +1,65 @@
+from moteinopy import MoteinoNetwork
+
+
+Moteinos = ['GreenDude']
+
+
+MoteinoCommands = {
+    'CorrectPasscode': 25,
+    'Status': 99,
+    'Test1': 1,
+    'Test2': 2,
+}
+
+MoteinoStructs = {
+    'GreenDude': "int Command;" + "byte Lights[7];" + "byte Temperature;",
+    'SplitFlap': "int Command;" + "char Letters[11];" + "byte Temperature;"
+}
+
+MoteinoIDs = {
+    'Base': 1,
+    'GreenDude': 11,
+    'SplitFlap': 101,
+    'Stealth': 7,
+    'TestDevice': 0
+}
+
+inv_MoteinoIDs = {v: k for k, v in MoteinoIDs.items()}
+
+
+class MyNetwork(MoteinoNetwork):
+    def receive(self, sender, diction):
+        print("Somtehing was received!!!!!!")
+
+
+# mynetwork = MyNetwork('/dev/ttyAMA0')
+mynetwork = MyNetwork('COM50')
+
+
+GreenDude = mynetwork.add_device(MoteinoIDs['GreenDude'],
+                                 MoteinoStructs['GreenDude'],
+                                 'GreenDude')
+GreenDude.add_translation('Command',
+                          ('Status', 99),
+                          ('Disp', 1101),
+                          ('ReceiveTalkingPillow', 42),
+                          ('TakeAwayTalkingPillow', 43),
+                          ('SetPassCode', 1102),)
+
+
+SplitFlap = mynetwork.add_device(MoteinoIDs['SplitFlap'],
+                                 MoteinoStructs['SplitFlap'],
+                                 'SplitFlap')
+SplitFlap.add_translation('Command',
+                          ('Status', 99),
+                          ('Disp', 10101),
+                          ('ClearAll', 10102))
+
+QuestList = [
+    'Elevator',
+    'LockPicking',
+    'GreenDude',
+    'WineCase',
+    'ShootingRange'
+]
+
--- ThePope.py
+++ ThePope.py
@@ -1,165 +1,41 @@
-import pyodbc
-from Dictionaries import *
-from Imports import *
-import Moteinos
-
-if not __name__ == "__main__":
-    raise Exception("ThePope must be run as a main module, no importing")
-
-GroupID = 0
-
-# proc2 = subprocess.Popen(['python', 'Asta.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-
-# StdoutLock = threading.Lock()
+from Config import *
+from HostInterface import startInterface
 
 
-# This static class was created to hold all reactions without clustering the global namespace
-class Reactions:
+def GreenDudeReceive(d):
+    print(d)
+
+GreenDude.bind(receive=GreenDudeReceive)
+
+
+class TalkingPillow(object):
     def __init__(self):
-        pass
+        self.Holder = None
 
-#   All Reactions must be defined here
-    @staticmethod
-    def no_reaction(incoming=None, event=None):
-        pass
+    def give(self, device):
+        assert self.Holder is None
+        self.Holder = device
+        device.send('ReceiveTheTalkingPillow')
 
-    @staticmethod
-    def test(incoming, event=None):
-        print("this is a test, the " + str(event.Name) +
-              " raised this. Also, " + str(incoming))
-
-    @staticmethod
-    def test2(incoming, event=None):
-        Moteinos.send({
-            'ID': event.ID,
-            'Command': 'Test2'
-        })
-
-    @staticmethod
-    def execute(incoming, event=None):
-        exec incoming['exec']
-
-    class Moteinos:
-        def __init__(self):
-            pass
-
-        class GreenDude:
-            def __init__(self):
-                pass
-
-            @staticmethod
-            def greendude_correct_passcode(incoming, event=None):
-                # play message
-                # start tape recorder
-                # let faceZ know about it
-                # stop tape recorder
-                raise NotImplementedError
-
-            handles = {
-                MoteinoCommands['CorrectPasscode']: greendude_correct_passcode
-            }
-
-            @staticmethod
-            def handle(incoming, event=None):
-                Reactions.Moteinos.GreenDude.handles[incoming['Command']].__func__(incoming, event=None)
-
-        class Test:
-            def __init__(self):
-                pass
-
-            @staticmethod
-            def test1(incoming, event=None):
-                debug_print("moteino test recieved " + str(incoming))
-
-            handles = {
-                MoteinoCommands['Test1']: test1
-            }
-
-            @staticmethod
-            def handle(incoming, event=None):
-                Reactions.Moteinos.Test.handles[incoming['Command']].__func__(incoming, event=None)
-
-        handles = {
-            MoteinoIDs['TestDevice']: Test.handle,
-            MoteinoIDs['GreenDude']: GreenDude
-        }
-
-        @staticmethod
-        def handle(incoming, event=None):
-            Reactions.Moteinos.handles[incoming['SenderID']](incoming, event=event)
+    def reclaim(self):
+        if self.Holder is not None:
+            self.Holder.send('GiveTheTalkingPillow')
+            self.Holder = None
 
 
-def report_info(event_id, info2log):
-        _cnxn = pyodbc.connect(r'Driver={SQL Server};'
-                               r'Server=.\SQLEXPRESS;'
-                               r'Database=KjallariDB;'
-                               r'Trusted_Connection=yes;')
-        _cursor = _cnxn.cursor()
-        for (s, i) in info2log:
-            _cursor.execute("EXEC ReportInfo %d, \'%s\', %d" % (event_id, s, i))
-        _cnxn.close()
+
+class Quest(object):
+    def __init__(self, next_quest):
+        self.NextQuest = next_quest
+
+    def cleanup(self):
+        talking_pillow.reclaim()
+        self.NextQuest.start()
 
 
-# A class describing the Event object,
-class Event(object):
-    def __init__(self, _id, name, react_fun, reportable=True):
-        self.React_fun = react_fun
-        self.ID = _id
-        self.Name = name
-        self.Reportable = reportable
-
-    def report(self):
-        _cnxn = pyodbc.connect(r'Driver={SQL Server};'
-                               r'Server=.\SQLEXPRESS;'
-                               r'Database=KjallariDB;'
-                               r'Trusted_Connection=yes;')
-        _cursor = _cnxn.cursor()
-        _cursor.execute("EXEC ReportEvent %d, %d" % (self.ID, GroupID))
-        _cnxn.close()
-
-    def fire(self, incoming=None):
-        if self.Reportable:
-            self.report()
-        self.React_fun(incoming, event=self)
+class Elevetor(Quest):
+    def __init__(self):
+        Quest.__init__(self)
 
 
-events = {
-    EventIDs['Execute']:
-        Event(EventIDs['Execute'], "Execute", Reactions.execute, reportable=False),
-    EventIDs['Test1']:
-        Event(EventIDs['Test1'], 'Test1', Reactions.test, reportable=False),
-    EventIDs['Test2']:
-        Event(EventIDs['Test2'], 'Test2', Reactions.test2, reportable=False),
-    EventIDs['RecievedFromMoteinoNetwork']:
-        Event(EventIDs['RecievedFromMoteinoNetwork'], "RecievedFromMoteinoNetwork", Reactions.Moteinos.handle)
-}
-
-
-class FireEventThread(threading.Thread):
-    def __init__(self, incoming):
-        threading.Thread.__init__(self)
-        self.Incoming = incoming
-
-    def run(self):
-        events[self.Incoming['ID']].fire(self.Incoming)
-
-
-class ProcessQueueThread(threading.Thread):
-    def __init__(self, q):
-        threading.Thread.__init__(self)
-        self.Q = q
-
-    def run(self):
-        while True:
-            diction = self.Q.get()
-            debug_print("ProcessQueueThread retrieved something from Q")
-            fire = FireEventThread(diction)
-            fire.start()
-
-
-if __name__ == "__main__":
-    Moteinos.start_listening()
-    Qthread = ProcessQueueThread(Moteinos.Q)
-    Qthread.start()
-
-#     eitthvad meira....
+startInterface()
