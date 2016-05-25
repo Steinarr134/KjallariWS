db5b8f5e5c172993fa71cb2dfd0edeadcbe22bef
Steinarr134 <steinarrrrrrrrrr@gmail.com>
2016-05-25 21:18:42 +0000

    added test7

---
+++ Test7.py
@@ -0,0 +1,16 @@
+import MoteinoPY as moteinopy
+
+mynetwork = moteinopy.MoteinoNetwork('/dev/ttyAMA0',
+                                     frequency=moteinopy.RF69_433MHZ,
+                                     network_id=7,
+                                     encryption_key="HugiBogiHugiBogi"
+                                     )
+
+GreenDude = mynetwork.add_device(11,  "unsigned int Command;byte Lights[7];byte Temperature;", "GreenDude")
+
+GreenDude.add_translation('Command',
+                          ('Status', 99),
+                          ('Disp', 1101),
+                          ('GiveTalkingPillow', 42),
+                          ('TakeAwayTalkingPillow', 43),
+                          ('SetPassCode', 1102),)
