9ce0c2d4d4028abe5064a0252a3912b3db5ebd8a
steinarr134 <steinarrrrrrrrrr@gmail.com>
2016-06-06 18:46:15 +0000

    Minor changes to Segulband

--- SegulBand/SegulBand.py
+++ SegulBand/SegulBand.py
@@ -1,59 +1,67 @@
 import RPi.GPIO as GPIO
 from arduino import Motor
 import atexit
+import sys
+import time
+
+print "running..."
+GPIO.setmode(GPIO.BCM)
+atexit.register(GPIO.cleanup)
 
 motor = Motor()
 
-# region Play Button
-play_button_pin = 5
+
+if True:
+
+    # region Play Button
+    play_button_pin = 5
 
 
-def play_button_press():
-    print "Play was pressed"
-    motor.play()
+    def play_button_press(channel):
+        print "Play was pressed"
+        motor.play()
 
-GPIO.setup(play_button_pin, GPIO.IN)
-GPIO.add_event_detect(play_button_pin, GPIO.FALLING, callback=play_button_press, bouncetime=300)
-# endregion
-
-
-# region stop Button
-stop_button_pin = 6
+    GPIO.setup(play_button_pin, GPIO.IN)
+    GPIO.add_event_detect(play_button_pin, GPIO.FALLING, callback=play_button_press, bouncetime=300)
+    # endregion
 
 
-def stop_button_press():
-    print "stop was pressed"
-    motor.stop()
-
-GPIO.setup(stop_button_pin, GPIO.IN)
-GPIO.add_event_detect(stop_button_pin, GPIO.FALLING, callback=stop_button_press, bouncetime=300)
-# endregion
+    # region stop Button
+    stop_button_pin = 6
 
 
-# region reverse Button
-reverse_button_pin = 13
+    def stop_button_press(channel):
+        print "stop was pressed"
+        #motor.stop()
+
+    GPIO.setup(stop_button_pin, GPIO.IN)
+    GPIO.add_event_detect(stop_button_pin, GPIO.FALLING, callback=stop_button_press, bouncetime=300)
+    # endregion
 
 
-def reverse_button_press():
-    print "reverse was pressed"
-    motor.rewind()
-
-GPIO.setup(reverse_button_pin, GPIO.IN)
-GPIO.add_event_detect(reverse_button_pin, GPIO.FALLING, callback=reverse_button_press, bouncetime=300)
-# endregion
+    # region reverse Button
+    reverse_button_pin = 13
 
 
-# region forward Button
-forward_button_pin = 19
+    def reverse_button_press(channel):
+        print "reverse was pressed"
+        motor.rewind()
+
+    GPIO.setup(reverse_button_pin, GPIO.IN)
+    GPIO.add_event_detect(reverse_button_pin, GPIO.FALLING, callback=reverse_button_press, bouncetime=300)
+    # endregion
 
 
-def forward_button_press():
-    print "forward was pressed"
-    motor.forward()
-
-GPIO.setup(forward_button_pin, GPIO.IN)
-GPIO.add_event_detect(forward_button_pin, GPIO.FALLING, callback=forward_button_press, bouncetime=300)
-# endregion
+    # region forward Button
+    forward_button_pin = 19
 
 
-atexit.register(GPIO.cleanup)
+    def forward_button_press(channel):
+        print "forward was pressed"
+        motor.forward()
+
+    GPIO.setup(forward_button_pin, GPIO.IN)
+    GPIO.add_event_detect(forward_button_pin, GPIO.FALLING, callback=forward_button_press, bouncetime=300)
+    # endregion
+
+time.sleep(100)
--- SegulBand/arduino.py
+++ SegulBand/arduino.py
@@ -1,11 +1,12 @@
 import serial
 from moteinopy import Struct
 from threading import Lock
-
+import time
 
 class Motor(object):
     def __init__(self):
-        self.Serial = serial.Serial('dev/ttyAMA0', baudrate=115200)
+        self.Serial = serial.Serial('/dev/ttyUSB0', baudrate=115200)
+        time.sleep(1)
         self.SerialLock = Lock()
         self.Struct = Struct("int Command;")
         self.Status = 99
@@ -17,28 +18,26 @@ class Motor(object):
         self.SetParams = 1306
         self.SetLights = 1307
 
-    def play(self):
+    def _send_command(self, command):
         with self.SerialLock:
-            self.Serial.write(self.Struct.encode({'Command': self.Play})+'\n')
+            self.Serial.write(self.Struct.encode({'Command': command}) + '\n')
+
+    def play(self):
+        self._send_command(self.Play)
 
     def stop(self):
-        with self.SerialLock:
-            self.Serial.write(self.Struct.encode({'Command': self.Stop})+'\n')
+        self._send_command(self.Stop)
 
     def rewind(self):
-        with self.SerialLock:
-            self.Serial.write(self.Struct.encode({'Command': self.Rewind})+'\n')
+        self._send_command(self.Rewind)
 
     def forward(self):
-        with self.SerialLock:
-            self.Serial.write(self.Struct.encode({'Command': self.FastForward})+'\n')
+        self._send_command(self.FastForward)
 
     def return2zero(self):
-        with self.SerialLock:
-            self.Serial.write(self.Struct.encode({'Command': self.ReturnToZero})+'\n')
+        self._send_command(self.ReturnToZero)
 
     def get_temps(self):
-        with self.SerialLock:
-            self.Serial.write(self.Struct.encode({'Command': self.Status})+'\n')
+        self._send_command(Status)
         incoming = self.Serial.readline()
         return self.Struct.decode(incoming)
