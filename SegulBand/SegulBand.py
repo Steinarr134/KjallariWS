33bb943d0e41e046f48d548f94529d0f70bdf41f
steinarr134 <steinarrrrrrrrrr@gmail.com>
2016-09-24 18:28:05 +0000

    segulband loksins

--- SegulBand/SegulBand.py
+++ SegulBand/SegulBand.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import RPi.GPIO as GPIO
 from arduino import Motor
 import atexit
@@ -15,175 +14,7 @@ motor = Motor()
 mixer.init()
 m = mixer.music
 
-CurrentFile = "C:\Users\SteinarrHrafn\Google Drive\Kjallari William Stephenssonar\Camp Z\Lygamælir\Tape_ogg\\1_audio_WELCOME_INTRODUCTION"
-m.load(CurrentFile)
-PosOffset = 0
-FileLength = 50
-ScrollSpeed = 2
-
-last_press = None
-last_press_time = None
-
-HaveReleasedEvent = threading.Event()
-
-class NoFinishFileThread(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self):
-        while True:
-            time.sleep(0.5)
-            if m.get_pos() < 0:
-                m.play(0, FileLength)
-                m.pause()
-
-noFinishFileThread = NoFinishFileThread()
-noFinishFileThread.start()
-
-
-def realpos():
-    global PosOffset
-    pos = m.get_pos()/float(1000)
-    return pos - PosOffset
-
-
-def until_end():
-    return FileLength - realpos()
-
-
-def skip(s):
-    if s > until_end():
-        print "to much of forward!!!"
-    elif -s > realpos():
-        print "too much rewind!!!"
-    else:
-        global PosOffset
-        m.set_pos(realpos() + s)
-        PosOffset -= s
-
-
-def play():
-    pass
-
-
-def stop():
-    pass
-
-
-def forward():
-    m.pause()
-    global last_press_time
-    last_press_time = time.time()
-    global last_press
-    last_press = 'forward'
-    _until_end = until_end()
-    HaveReleasedEvent.clear()
-    if not HaveReleasedEvent.wait(_until_end/float(ScrollSpeed)):
-        HaveReleasedEvent.set()
-        skip(_until_end)
-
-
-def rewind():
-    m.pause()
-    global last_press_time
-    last_press_time = time.time()
-    global last_press
-    last_press = 'rewind'
-    _until_end = until_end()
-    HaveReleasedEvent.clear()
-    if not HaveReleasedEvent.wait(_until_end/float(ScrollSpeed)):
-        HaveReleasedEvent.set()
-        skip(_until_end)
-
-
-def release(channel):
-    if not HaveReleasedEvent.is_set():
-        HaveReleasedEvent.set()
-        global last_press
-        global last_press_time
-        if last_press == 'forward':
-            skip((time.time() - last_press_time)*ScrollSpeed)
-        elif last_press == 'rewind':
-            skip((last_press_time - time.time())*ScrollSpeed)
-        else:
-            print "noting last_press WTF!"
-
-        m.unpause()
-        last_press = None
-
-
-# region stop Button
-stop_button_pin = 6
-
-
-def stop_button_press(channel):
-    print "stop was pressed"
-    stop()
-
-GPIO.setup(stop_button_pin, GPIO.IN)
-GPIO.add_event_detect(stop_button_pin, GPIO.FALLING, callback=stop_button_press, bouncetime=300)
-# endregion
-
-
-# region reverse Button
-reverse_button_pin = 13
-
-
-def reverse_button_press(channel):
-    print "reverse was pressed"
-    rewind()
-
-GPIO.setup(reverse_button_pin, GPIO.IN)
-GPIO.add_event_detect(reverse_button_pin, GPIO.FALLING, callback=reverse_button_press, bouncetime=300)
-# endregion
-
-
-# region forward Button
-forward_button_pin = 19
-
-
-def forward_button_press(channel):
-    print "forward was pressed"
-    forward()
-
-GPIO.setup(forward_button_pin, GPIO.IN)
-GPIO.add_event_detect(forward_button_pin, GPIO.FALLING, callback=forward_button_press, bouncetime=300)
-# endregion
-
-# region Play Button
-play_button_pin = 5
-
-
-def play_button_press(channel):
-    print "Play was pressed"
-    play()
-
-GPIO.setup(play_button_pin, GPIO.IN)
-GPIO.add_event_detect(play_button_pin, GPIO.FALLING, callback=play_button_press, bouncetime=300)
-GPIO.add_event_detect(forward_button_pin, GPIO.RISING, callback=release, bouncetime=300)
-GPIO.add_event_detect(reverse_button_pin, GPIO.RISING, callback=release, bouncetime=300)
-
-# endregion
-
-time.sleep(100)
-=======
-import RPi.GPIO as GPIO
-from arduino import Motor
-import atexit
-import time
-from pygame import mixer
-import threading
-
-print "running..."
-GPIO.setmode(GPIO.BCM)
-atexit.register(GPIO.cleanup)
-
-motor = Motor()
-
-mixer.init()
-m = mixer.music
-
-CurrentFile = "1_audio_WELCOME_INTRODUCTION.ogg"
+CurrentFile = "audio_files/1_audio_WELCOME_INTRODUCTION.ogg"
 m.load(CurrentFile)
 m.play()
 m.pause()
@@ -195,6 +26,7 @@ last_press = None
 last_press_time = None
 something_is_being_pressed = False
 playing_active = False
+time.sleep(1)
 motor.set_lights(35)
 
 HaveReleasedEvent = threading.Event()
@@ -208,7 +40,7 @@ class NoFinishFileThread(threading.Thread):
             time.sleep(0.5)
             if m.get_pos() < 0:
                 m.play(0, FileLength)
-                m.pause()
+                stop()
 
 noFinishFileThread = NoFinishFileThread()
 noFinishFileThread.start()
@@ -236,6 +68,8 @@ def skip(s):
 
 
 def play():
+    if abs(realpos() - FileLength) < 0.05:
+        return
     m.unpause()
     global playing_active
     playing_active = True
@@ -261,6 +95,7 @@ def forward():
     else:
         something_is_being_pressed = True
     _until_end = until_end()
+    print "until end: " + str(_until_end)
     motor.forward()
     HaveReleasedEvent.clear()
     print "waiting for release event"
@@ -268,6 +103,7 @@ def forward():
         print "Event.wait timeout occured"
         HaveReleasedEvent.set()
         skip(_until_end)
+        stop()
     else:
         print "waiting ended"
 
@@ -290,6 +126,7 @@ def rewind():
         print "Event.wait timeout occured"
         HaveReleasedEvent.set()
         skip(-_until_beginning)
+        motor.stop()
     else:
         print "waiting ended"
 
@@ -318,8 +155,7 @@ def release():
         stop()
 
 # region stop Button
-stop_button_pin = 6
-print "dsaf`2"
+stop_button_pin = 13
 
 def stop_button_press(channel):
     print "stop was pressed"
@@ -342,9 +178,8 @@ class ExecutionThread(threading.Thread):
         self.fun()
 
 # region reverse Button
-reverse_button_pin = 13
+reverse_button_pin = 6
 
-print "hernar lika"
 def reverse_button_press(channel):
     time.sleep(0.05)
     if not GPIO.input(reverse_button_pin):
@@ -367,7 +202,7 @@ GPIO.add_event_detect(reverse_button_pin,
 
 
 # region forward Button
-forward_button_pin = 19
+forward_button_pin = 5
 
 
 def forward_button_press(channel):
@@ -391,7 +226,7 @@ GPIO.add_event_detect(forward_button_pin,
 # endregion
 
 # region Play Button
-play_button_pin = 5
+play_button_pin = 19
 
 
 def play_button_press(channel):
@@ -409,4 +244,3 @@ GPIO.add_event_detect(play_button_pin,
 # endregion
 
 print "Steinarr kann ad laga forrit sem virka ekki"
->>>>>>> refs/remotes/origin/segulband2
