ab8aa3191de6af77af358e4ce825ae407f9d6b10
Steinarr134 <steinarrrrrrrrrr@gmail.com>
2018-02-08 19:13:01 +0000

    TV sound fixed
    by forcing raspi audio to hdmi to free analog soundcard from oygame
    and adding "-o local" to omxplayer command

--- TvPi/TvMain.py
+++ TvPi/TvMain.py
@@ -1,15 +1,17 @@
-import os
+import subprocess
 import pygame
 import time
-from moteinopy import MoteinoNetwork
 import sys
+sys.path.append("/home/pi/moteinopy")
+from moteinopy import MoteinoNetwork
 import logging
+import threading
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
-sys.stderr=open("/home/pi/logs/" + time.strftime("%c") + ".txt", 'w+')
+# sys.stderr=open("/home/pi/logs/" + time.strftime("%c") + ".txt", 'w+')
 
-videofolder = "/home/pi/Videos/"
+videofolder = "/home/pi/usbdrv/"
 
 
 PLAYEVENT = pygame.USEREVENT + 1
@@ -19,13 +21,15 @@ def handle_command(stuff):
         stuff = dict(stuff)
     if stuff['Command'] == "Play":
         print "putting in play event"
-        play_event = pygame.event.Event(PLAYEVENT,
+        play_event = pygame.fastevent.Event(PLAYEVENT,
                                         message=videofolder + stuff['What2Play'].rstrip('\0').strip())
-        pygame.event.post(play_event)
+        pygame.fastevent.post(play_event)
     elif stuff["Command"] == "Status":
         Pope.send("Status")
 
-mynetwork = MoteinoNetwork("/dev/ttyUSB0", base_id=41, init_base=False)
+print "starting network"
+mynetwork = MoteinoNetwork("/dev/ttyUSB0", base_id=41, init_base=False, baudrate=115200)
+mynetwork.logger.setLevel(logging.DEBUG)
 Pope = mynetwork.add_node(1, "int Command;char What2Play[10];", "Pope")
 Pope.add_translation("Command",
                      ('Play', 4101),
@@ -33,25 +37,61 @@ Pope.add_translation("Command",
 Pope.bind(receive=handle_command)
 
 pygame.init()
+pygame.fastevent.init()
 pygame.mouse.set_visible(False)
 
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+#screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((500, 500))
 
 screen.fill((0, 0, 0))
 
+
+class PopenThread(threading.Thread):
+    def __init__(self, command):
+        threading.Thread.__init__(self)
+        self.Command = command
+        self.Running = None
+        self.start()
+        self.Done = False
+
+    def run(self):
+        self.Running = subprocess.Popen(self.Command.split(" "), stdin=subprocess.PIPE)
+
+    def die(self):
+        if self.Running.poll() is None:
+            #print "Killing myself #######################"
+            self.Running.stdin.write('q')
+            time.sleep(0.1)
+        else:
+            #print "444444444444444444 I'm already dead, bith!"
+            pass
+        self.Done = True
+        
+Threads = []
+
 done = False
 while not done:
-    for event in pygame.event.get():
+    time.sleep(0.1)
+    t2remove = []
+    for thread in Threads:
+        if thread.Done:
+            t2remove.append(thread)
+    for thread in reversed(t2remove):
+        #print "================= removing thread"
+        Threads.remove(thread)
+            
+    for event in pygame.fastevent.get():
         if event.type == pygame.QUIT:
             done = True
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 done = True
-            elif event.key == pygame.K_a:
-                os.popen("omxplayer small.mp4")
         elif event.type == PLAYEVENT:
-            print("Play event, doing: " + "omxplayer " + event.message)
-            os.popen("omxplayer " + event.message)
+            for thread in Threads:
+                thread.die()
+            #print("---------------------Play event, doing: " + "omxplayer " + event.message)
+            Threads.append(PopenThread("omxplayer " + event.message + " -o local"))
+        
 
 pygame.quit()
 logging.debug("End of script")
