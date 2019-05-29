2aab50fe1e966f0201b0e354332e8a90b2588ada
ingiv <skallapopp@gmail.com>
2019-05-29 21:14:16 +0000

    Add files via upload

--- MoteinoSketches/StealthSensor/StealthSensor.ino
+++ MoteinoSketches/StealthSensor/StealthSensor.ino
@@ -9,12 +9,11 @@
 #include <SharpIR.h>
 // https://github.com/guillaume-rico/SharpIR
 
-#define threshold 30  // Threshold for triggering
-#define rel 8         // Relay used for testing
+#define threshold 70  // Threshold for triggering
+#define rel 3         // Relay used for testing
 #define ledPin 13     // Visual aid
-#define IR A0         // the pin where your sensor is attached
+#define IR A7         // the pin where your sensor is attached
 #define model 1080    // used 1080 because model GP2Y0A21YK0F is used
-
 SharpIR SharpIR(IR, model);
 
 void setup() {
@@ -23,22 +22,21 @@ void setup() {
  pinMode (ledPin, OUTPUT);  // Test LED
 }
 
-
 void loop() {
   delay(70);                      // Signal stabilizing (deBounce)
   int dis=SharpIR.distance();     // this returns the distance to the object you're measuring
-//  Serial.print("distance: ");   // prints to the serial monitor
-//  Serial.println(dis);          // prints value to serial monitor
+  Serial.print("distance: ");   // prints to the serial monitor
+  Serial.println(dis);          // prints value to serial monitor
 
 //---------- DO SOMETHING ---------------//
-  int i;
-  if (dis < threshold){           // distance less than X cm
-    for (i=0; i<10; i++){         // BLINKY BLINKY
-      digitalWrite (ledPin, HIGH);
-      delay(100);
-      digitalWrite (ledPin, LOW);
-      delay(100);
-    }     // end of for loop
+ 
+  digitalWrite(rel, LOW);
+  digitalWrite(ledPin, LOW);
+  if (dis < threshold){           // distance less than X cm  
+   digitalWrite(rel, HIGH);
+   digitalWrite(ledPin, HIGH);
+   Serial.println("!!!!!!!!OUTPUT______ACTIVE!!!!!!!!!!");          // prints value to serial monitor
   }       // end of if loop
+  
 //----------------------------------------//     
 } // end of code
