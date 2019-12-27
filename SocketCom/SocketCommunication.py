a448d004dc33beeb8157076861e2373860629b77
Steinarr134 <steinarrrrrrrrrr@gmail.com>
2019-12-27 22:12:05 +0000

    testfix1

--- SocketCom/SocketCommunication.py
+++ SocketCom/SocketCommunication.py
@@ -10,6 +10,19 @@ import threading
 PORT = 3010
 
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+
 class Server(SocketServer.TCPServer):
     def __init__(self, handle_shit, port=PORT):
 
@@ -25,7 +38,7 @@ class Server(SocketServer.TCPServer):
                     # print data
                     handle_shit(data.strip())
 
-        address = ("localhost", port)
+        address = (get_ip(), port)
         SocketServer.TCPServer.__init__(self, address, RequestHandler)
 
         self.Thread = threading.Thread(target=self.serve_forever)
@@ -50,7 +63,8 @@ class Client(object):
             try:
                 self._connect()
                 self.s.send(str(shit))
-            except socket.error:
+            except socket.error as e:
+                print "had a problem: " + str(e)
                 return
 
     def _connect(self):
